9a46d6c2cd337a7d982f21397998593fe637242a
fuzeyu <948290638@qq.com>
2020-06-01 10:20:02 +0800

    Revert "Background interface"
    This reverts commit d6c2c90abee2fc9b2f9829f4b4c302cc754cc2dd.

--- backgroud interface/public/backgroudinterface/interface/delusertext.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_POST['id'];
-
-$con = new mysqli("localhost", "xiao-yufei", "D5I1F0D9wJAEFNmKuK4au6gc","2201613130107");
-$delete = "DELETE FROM user WHERE id='$id'";
-$do = mysqli_query($con, $delete);
-header("Location:/xyf/public/backgroudinterface/customer.php");
-mysqli_close($con); 
-?>
